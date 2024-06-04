0b32642166be505f5add74dc08c0b68b41a61270
Prakriti <timalsena.prakriti97@gmail.com>
2024-06-04 16:45:21 +0545

    feat : create responsive landing page

---
+++ script.js
@@ -0,0 +1,33 @@
+const btn = document.getElementById("mode-btn");
+var darkMode = localStorage.getItem("darkMode");
+const parentListEl = document.querySelector(".header__nav-item");
+const downloadBtn = document.getElementById("download-btn");
+
+const enableDarkMode = () => {
+    btn.src = "Images/emoji.png";
+    downloadBtn.src = "Images/download.png";
+    document.body.classList.add('dark-theme');
+    localStorage.setItem("darkMode", 'enabled');
+};
+
+const disableDarkMode = () => {
+    btn.src = "Images/emoji-light.png";
+    downloadBtn.src = "Images/download_dark.png";
+    document.body.classList.remove("dark-theme");
+    localStorage.setItem("darkMode", null);
+};
+
+if(darkMode === 'enabled') {
+    enableDarkMode();
+} else {
+    disableDarkMode();
+}
+
+btn.addEventListener("click", () => {
+    darkMode = localStorage.getItem("darkMode");
+    if(darkMode !== 'enabled') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+});
