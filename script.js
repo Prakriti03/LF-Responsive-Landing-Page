1f79f31ecd9d75cf037748f8fc2db50de2b83a06
Prakriti <timalsena.prakriti97@gmail.com>
2024-06-05 21:31:00 +0545

    add styling

--- script.js
+++ script.js
@@ -4,30 +4,30 @@ const parentListEl = document.querySelector(".header__nav-item");
 const downloadBtn = document.getElementById("download-btn");
 
 const enableDarkMode = () => {
-    btn.src = "Images/emoji.png";
-    downloadBtn.src = "Images/download.png";
-    document.body.classList.add('dark-theme');
-    localStorage.setItem("darkMode", 'enabled');
+  btn.src = "Images/emoji.png";
+  downloadBtn.src = "Images/download.png";
+  document.body.classList.add("light-theme");
+  localStorage.setItem("darkMode", "enabled");
 };
 
 const disableDarkMode = () => {
-    btn.src = "Images/emoji-light.png";
-    downloadBtn.src = "Images/download_dark.png";
-    document.body.classList.remove("dark-theme");
-    localStorage.setItem("darkMode", null);
+  btn.src = "Images/emoji-light.png";
+  downloadBtn.src = "Images/download_dark.png";
+  document.body.classList.remove("light-theme");
+  localStorage.setItem("darkMode", null);
 };
 
-if(darkMode === 'enabled') {
-    enableDarkMode();
+if (darkMode === "enabled") {
+  enableDarkMode();
 } else {
-    disableDarkMode();
+  disableDarkMode();
 }
 
 btn.addEventListener("click", () => {
-    darkMode = localStorage.getItem("darkMode");
-    if(darkMode !== 'enabled') {
-        enableDarkMode();
-    } else {
-        disableDarkMode();
-    }
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
 });
